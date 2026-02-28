473d056c316e4a51d2f7a8f64274fb1876bb150b
Daksh Patel <dakshpatel1520@gmail.com>
2026-02-28 15:11:11 +0530

    refactor banner components to use reusable banner section

--- src/components/quality-policy/banner/index.jsx
+++ src/components/quality-policy/banner/index.jsx
@@ -1,33 +1,10 @@
 "use client"
 
 import React from "react"
-import BrochuresImage from "@/assets/images/brochures.png"
-import Image from "next/image"
+import ReuseBannerSection from "@/components/reuse-banner"
 
 const BannerSection = () => {
-  return (
-    <section className='relative w-full h-60 overflow-hidden'>
-      <Image
-        src={BrochuresImage}
-        alt='Brochure Image'
-        fill
-        priority
-        className='object-cover'
-      />
-
-      {/* Green Overlay */}
-      <div className='absolute inset-0 bg-[#28a745]/80'></div>
-
-      {/* Content */}
-      <div className='absolute inset-0 flex items-center justify-between'>
-        <div className='container flex items-center justify-between'>
-          <h1 className='text-white text-4xl md:text-4xl font-semibold max-w-3xl leading-snug'>
-            Quality-Policy
-          </h1>
-        </div>
-      </div>
-    </section>
-  )
+  return <ReuseBannerSection title={"Quality-policy"} />
 }
 
 export default BannerSection
