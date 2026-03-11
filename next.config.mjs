ef6a293de67276288c50033eeb35c6fd00c8c006
Daksh Patel <dakshpatel1520@gmail.com>
2026-03-11 21:41:18 +0530

    refactor next.config.mjs; standardize formatting for consistency

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  distDir: "dist", // 👈 change folder name here
+  distDir: "dist",
   images: {
-    unoptimized: true, // 👈 VERY IMPORTANT
+    unoptimized: true,
   },
 }
 
